01d4e248ef909555f51a6e34b036e6cfc3aa6354
luist <luistrigueiros@gmail.com>
2026-02-01 11:56:03 +0000

    feature: Removed core.py

--- src/postgres_setup/core.py
+++
@@ -1,9 +0,0 @@
-
-import subprocess
-
-from .commands import Command
-
-
-class PostgresDevSetup(Command):
-    def __init__(self):
-        super().__init__(name="__postgres_dev_setup__", description="Internal helper")
